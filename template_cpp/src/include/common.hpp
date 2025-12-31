a5720aa1bad9a8e3b71878e46442ef15286eed24
ynishimi <nishimi.yuki.37z@st.kyoto-u.ac.jp>
2025-12-31 12:44:12 +0100

    fix: output format and disable debugPrint

--- template_cpp/src/include/common.hpp
+++ template_cpp/src/include/common.hpp
@@ -13,15 +13,15 @@ template <typename T>
 std::ostream &operator<<(std::ostream &out, const std::set<T> &set)
 {
     if (set.empty())
-        return out << "{}";
-    out << "{ " << *set.begin();
+        return out << "";
+    out << *set.begin();
     std::for_each(std::next(set.begin()), set.end(), [&out](const T &element)
-                  { out << ", " << element; });
-    return out << " }";
+                  { out << " " << element; });
+    return out << "";
 }
 
 template <typename T>
 void debugPrint(const string &label, const T &debug_data)
 {
-    cout << "[DEBUG] " << label << ": " << debug_data << endl;
+    // cout << "[DEBUG] " << label << ": " << debug_data << endl;
 }
